5825279a8837897fc9a5fd831df8dbdbe8afe6b0
Majid El Achy <majid.achy.ma@gmail.com>
2024-10-15 23:07:36 +0300

    Create Client and Transaction Model and their Migrations

--- neoApi/transactions/admin.py
+++ neoApi/transactions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Client,Transaction
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Transaction)
--- neoApi/transactions/models.py
+++ neoApi/transactions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    client_id = models.IntegerField(primary_key= True)
+    name = models.CharField()
+    email = models.CharField()
+    date_of_birth = models.DateField()
+    city = models.CharField()
+    state = models.CharField()
+    country = models.CharField()
+    account_balance = models.DecimalField(max_digits=8, decimal_places= 2)
+
+class Transaction(models.Model):
+    transaction_id = models.IntegerField(primary_key= True)
+    client_id = models.IntegerField()
+    transaction_type = models.CharField()
+    transaction_date = models.DateTimeField()
+    amount = models.DecimalField(max_digits=7, decimal_places= 2)
+    currency = models.CharField()
