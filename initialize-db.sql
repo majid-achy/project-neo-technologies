7b8a234c6f5521336df9438dad5751ebac8ae614
Majid El Achy <majid.achy.ma@gmail.com>
2024-10-18 01:34:23 +0300

    Add Initialization Script and Requirments file

---
+++ initialize-db.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE "neo-project"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+CREATE ROLE testuser WITH
+  LOGIN
+  SUPERUSER
+  INHERIT
+  CREATEDB
+  CREATEROLE
+  REPLICATION
+  BYPASSRLS
+  PASSWORD 'test';
